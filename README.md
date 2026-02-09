5c4a17977377bbfd45c9cc8b57e471c28541fcc1
batool <batooola.97@gmail.com>
2026-02-09 16:43:45 +0300

    monday

--- README.md
+++ README.md
@@ -6,13 +6,12 @@
 
 ##### data: 2/5/2026
 
-# the game look like this :
+# the game look like this :(1)
 ![alt text](Screenshot_20260205_155354_Sudoku.jpg)
 
 # discretion the game :
 
 - But the right number that make line and the squ that have num from 1 to 9 without  repeat.
-
 
 
  #### * not repeat the num in the 
@@ -38,8 +37,16 @@ board ={
     [1,0,0,0,0,9,5,7,0]
 
 }
+ 
+* take the number form the board and put this number in the sq without 0 value and if 0 put function click and choose the number from card under the board. 
 
- * if the board =  answer
+* and chick the number from board  if = to the answer .....trus 
+* and if wrong .....error (in the css red in the sq(number)) 
+
+* the button make reset 
+
+
+
 ##  function:
 - 
 - function game() =  {
@@ -54,6 +61,21 @@ for(let i=0; i< 9 ; i++) {
 
 - 
 
-## the answer:
+## the answer:(1)
 ![alt text](Screenshot_20260205_155915_Sudoku.jpg)
+
+
+
+## to improve the game:
+- hard ,easy ,mid (i will put from the sudoku)all number.
+- light when i click the number
+- choose the answer in different color 
+- 9 squares
+
+Monday: work on the css
+write the function in the javascript.
+
+### source:
+- (1) [idea and picture] sudoku game from my phone 
+
 
--- script.js
+++ script.js
@@ -1,43 +1,89 @@
 
-
 /// the board without the answer
-
-let board ={
+   
+let board =[
     [5,7,2,9,1,0,0,0,0],
-    [8,6,1,4,2,7,9,3,5],
-    [9,0,0,0,0,5,0,0,0],
-    [0,0,0,0,8,0,6,0,2],
-    [5,2,0,0,0,0,0,8,1],
-    [7,0,6,0,9,0,0,0,0],
-    [0,0,0,8,0,0,0,0,6],
-    [8,0,5,0,0,0,0,1,0], 
-    [1,0,0,0,0,9,5,7,0]
+    [0,6,0,0,0,0,9,0,5],
+    [9,0,4,0,5,8,0,0,0],
 
-}
+    [0,0,0,7,6,2,5,9,8],
+    [0,0,8,5,4,0,3,2,6],
+    [0,2,0,8,0,9,1,4,7],
 
+    [2,0,6,0,7,5,0,1,9],
+    [3,8,0,1,9,6,4,5,0],
+    [1,5,9,2,8,0,7,0,3],
+];
 // the answer
 
-let answer = {
+let answer = [
     [5,7,2,9,1,3,6,8,4],
     [8,6,1,4,2,7,9,3,5],
-    [9,0,0,0,0,5,0,0,0],
-    [0,0,0,0,8,0,6,0,2],
-    [5,2,0,0,0,0,0,8,1],
-    [7,0,6,0,9,0,0,0,0],
-    [0,0,0,8,0,0,0,0,6],
-    [8,0,5,0,0,0,0,1,0], 
-    [1,0,0,0,0,9,5,7,0]
+    [9,3,4,6,5,8,2,7,1],
 
+    [4,1,3,7,6,2,5,9,8],
+    [7,9,8,5,4,1,3,2,6],
+    [6,2,5,8,3,9,1,4,7],
+
+    [2,4,6,3,7,5,8,1,9],
+    [3,8,7,1,9,6,4,5,2], 
+    [1,5,9,2,8,4,7,6,3]
+
+];
+
+cardss  =[1,2,3,4,5,6,7,8,9]
+
+let squ = document.getElementById('squ')
+let card = document.querySelector('.card');
+let sq = document.querySelector('.sq');
+let c = document.querySelectorAll('.c');
+const h1 = document.querySelector('#h1')
+
+
+
+for (let i=1; i<9; i++){
+    for (let j=1; j<9; j++){
+         sq.textContent = board[i][j];
+
+    }}
+
+
+    /* c.onclick = function(){
+    num = sq.value;
+        sq.textContent = num}
+
+ */
+
+
+for (let i=0; i<9; i++){
+    for (let j=0; j<9; j++){
+
+
+        if (board[i][j]===0) {
+            sq.onclick = function(){
+                // choose the number from card  
+
+             let cards = c.value;
+             return;
+
+        }
+    }
+    else { 
+        sq.innerHTML = board[i][j];
+        }
+}}
+  
+ 
+// see if ..true 
+
+for (let i=0; i<9; i++){
+    for (let j=0; j<9; j++){
+
+if (board[i][j]=== answer[i][j]){
+      return
 }
-
-
-for(let i=0; i<board.length; i++) {
-
-    for(let j=0; j<board[i].length; j++) {
-    if (0=== ){
-     if (board[i] === answer[i]){
-
-
-}}}}
-
+else {
+    h1.innerHTML= 'wrong';
+}
+    }}
 
